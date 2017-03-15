537d09260b06a0159935fc9a424e9e1457fcc48c
M3103dB13 <titouandupuget@gmail.com>
2017-03-15 14:49:12 +0100

    Add files via upload

--- tcpclient2.py
+++ tcpclient2.py
@@ -5,7 +5,7 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 #connectionSocket.send("send it")
 message = raw_input("Input password:")
-clientSocket.send("message")
+clientSocket.send(message)
 modifiedSentence = clientSocket.recv(1024)
 print "From Server:", modifiedSentence
 clientSocket.close()
--- tcpserver2.py
+++ tcpserver2.py
@@ -1,4 +1,5 @@
 from socket import *
+passattendu = "hey"
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
@@ -6,9 +7,9 @@ serverSocket.listen(1)
 print "The server is ready to receive"
 while 1:
 	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024)
+	sentence = connectionSocket.recv(1024) 
 	capitalizedSentence = sentence.upper()
-
+	print (capitalizedSentence)
 	serverSocket2 = socket(AF_INET,SOCK_STREAM)
 	serverSocket2.bind(("",13000))
 	serverSocket2.listen(1)
@@ -16,7 +17,10 @@ while 1:
 	while 1 :
 		connectionSocket2, addr = serverSocket2.accept()
 		modSentence = connectionSocket2.recv(1024)
-		if modSentence == "hey" :
-
+		print (modSentence)
+		if modSentence == passattendu :
 			connectionSocket2.send(capitalizedSentence)
+			connectionSocket2.close()
+		if modSentence != passattendu :  
+			connectionSocket2.send("MDP Faux")
 			connectionSocket2.close()
